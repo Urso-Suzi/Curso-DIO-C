614c55921af12033bddad2c9ed7471b75c59e5f4
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-11-29 13:07:21 -0300

    manipulando variáveis

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -1,15 +1,80 @@
 ﻿using Estudo_C_.Models;
 
-Pessoa p = new Pessoa();
+// Pessoa p = new Pessoa();
 
-p.Nome = "Ricardo";
-p.Idade = 21;
-p.Cidade = "Fortaleza";
-p.Apresentar();
+// p.Nome = "Ricardo";
+// p.Idade = 21;
+// p.Cidade = "Fortaleza";
+// p.Apresentar();
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Jeovan";
-p1.Idade = 45;
-p1.Cidade = "São Paulo";
-Console.WriteLine(p1.Nome);
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Jeovan";
+// p1.Idade = 45;
+// p1.Cidade = "São Paulo";
+// Console.WriteLine(p1.Nome);
 
+// string apresentacao = "Olá, Seja bem Vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.50;
+
+// decimal preco = 1.50M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura);
+// Console.WriteLine("Valor da variável preco: " + preco.ToString());
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(2);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c += 5; // c = c + 5;
+
+// Console.WriteLine(c);
+
+// c -= 5;
+
+// Console.WriteLine(c);
+
+
+
+// Cast ou Casting, converter string para inteiro
+
+//string cinco = "5";
+//int a = Convert.ToInt32(null);
+
+// int a = int.Parse(null);
+
+// Console.WriteLine(a);
+
+// int inteiro = 5;
+
+// string a = inteiro.ToString();
+// Console.WriteLine(a);
+
+
+// double a = 5;
+// int b = Convert.ToInt32(a);
+
+
+// long a = long.MaxValue;
+
+// int b = Convert.ToInt32(a);
+
+// Console.WriteLine(b);
+
+
+double a =  2 / 4 + (5 - 1);
+
+Console.WriteLine(a);
