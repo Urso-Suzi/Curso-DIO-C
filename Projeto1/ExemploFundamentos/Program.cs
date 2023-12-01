2096e0d2f0ef9fd2f26680bece16e8e634ac990b
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-12-01 13:03:56 -0300

    Array

---
+++ Projeto1/ExemploFundamentos/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+int arrayInteiros = new int [3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+
+for(int contador = 0; contador < arrayInteiros.length; contador++)
+{
+    Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+}
