82562ed4c117da7812f3e18b5c8c3805e2327e3a
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-12-01 19:52:07 -0300

    copiando arrays

--- Projeto1/ExemploFundamentos/Program.cs
+++ Projeto1/ExemploFundamentos/Program.cs
@@ -1,12 +1,31 @@
 ﻿
 
-int arrayInteiros = new int [3];
+int[] arrayInteiros = new int[3];
 
 arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 
-for(int contador = 0; contador < arrayInteiros.length; contador++)
+
+//Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+int [] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+
+
+Console.WriteLine("Percorrendo um array com for");
+for(int contador = 0; contador < arrayInteiros.Length; contador++)
 {
-    Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
-}
+     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+ }
+
+
+
+// int contadorForEach = 0;
+// Console.WriteLine("Percorrendo um array com foreach");
+// foreach(int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição N° {contadorForEach} - {valor}");
+//     contadorForEach++;
+// }
