b50b3e7a592909d9e8364f09cfa46655b1e42d99
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-12-01 12:54:36 -0300

    alteração de pasta

--- Projeto1/ProjetoTest/Program.cs
+++ Projeto1/ProjetoTest/Program.cs
@@ -1,45 +1,57 @@
 ﻿using Estudo_C_.Models;
 
 
-string opcao;
-bool exibirMenu = true;
 
-while(exibirMenu)
-{
-    Console.Clear();
-    Console.WriteLine("Digite a sua opção");
-    Console.WriteLine("1 - Cadastras cliente");
-    Console.WriteLine("2 - Buscar cliente");
-    Console.WriteLine("3 - Apagar cliente");
-    Console.WriteLine("4 - Encerrar");
 
-    opcao = Console.ReadLine();
 
-    switch(opcao)
-    {
-        case"1":
-            Console.WriteLine("Cadastro de cliente");
-            break;
-        case"2":
-            Console.WriteLine("Busca de cliente");
-            break;
-        case"3":
-            Console.WriteLine("Apagar cliente");
-            break;
-        case"4":
-            Console.WriteLine("Encerrar");
-            //Environment.Exit(0);
-            exibirMenu = false;
-            break;
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
 
-    }
 
-}
 
-Console.WriteLine("O programa fechou");
+
+
+
+
+
+
+// string opcao;
+// bool exibirMenu = true;
+
+// while(exibirMenu)
+// {
+//     Console.Clear();
+//     Console.WriteLine("Digite a sua opção");
+//     Console.WriteLine("1 - Cadastras cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case"1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
+//         case"2":
+//             Console.WriteLine("Busca de cliente");
+//             break;
+//         case"3":
+//             Console.WriteLine("Apagar cliente");
+//             break;
+//         case"4":
+//             Console.WriteLine("Encerrar");
+//             //Environment.Exit(0);
+//             exibirMenu = false;
+//             break;
+//         default:
+//             Console.WriteLine("Opção inválida");
+//             break;
+
+//     }
+
+// }
+
+// Console.WriteLine("O programa fechou");
 
 
 
