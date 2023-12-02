bf9bdf8f86e836948d361dbb872bb127cf43ba24
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-12-02 15:16:32 -0300

    Listas

--- Projeto1/ExemploFundamentos/Program.cs
+++ Projeto1/ExemploFundamentos/Program.cs
@@ -1,24 +1,75 @@
 ﻿
 
-int[] arrayInteiros = new int[3];
-
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 64;
-arrayInteiros[2] = 50;
 
 
-//Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+List<string> listaString = new List<string>();
 
-int [] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
-Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
 
 
-
-Console.WriteLine("Percorrendo um array com for");
-for(int contador = 0; contador < arrayInteiros.Length; contador++)
+Console.WriteLine("Percorrendo com For");
+for(int i = 1; i < listaString.Count; i++)
 {
-     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
- }
+    Console.WriteLine($"Posição N° {i} - {listaString[i]}");
+}
+
+Console.WriteLine("Percorrendo com ForEach");
+int contadorForEach = 0;
+foreach(string item in listaString)
+{
+    Console.WriteLine($"Posição N° {contadorForEach} - {item}");
+    contadorForEach++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int[] arrayInteiros = new int[3];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+
+
+// //Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+// int [] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+
+
+// Console.WriteLine("Percorrendo um array com for");
+// for(int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//      Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+//  }
 
 
 
