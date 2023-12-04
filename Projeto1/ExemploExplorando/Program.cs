3ee8bc8b4d32a2d0f45df1c689bb1ba8deddcdae
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-12-04 04:40:33 -0300

    datetime e tryparse

--- Projeto1/ExemploExplorando/Program.cs
+++ Projeto1/ExemploExplorando/Program.cs
@@ -1,4 +1,81 @@
 ﻿using ExemploExplorando.Models;
+using System.Globalization;
+
+
+String dataString = "2022-12-17 18:00";
+
+bool sucesso = DateTime.TryParseExact(dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
+
+if(sucesso)
+{
+    Console.WriteLine($"Conversão com sucesso Data: {data}");
+}
+else
+{
+    Console.WriteLine($"{dataString} não é uma datas válida");
+}
+
+
+Console.WriteLine(data);
+
+
+
+// Console.WriteLine(data.ToShortDateString());
+// Console.WriteLine(data.ToShortTimeString());
+
+
+
+
+
+
+
+
+
+// Console.WriteLine(data.ToString("dd/mm/yyyy HH:mm"));
+// Console.WriteLine(data.ToString("mm/dd/yyyy"));
+// Console.WriteLine(data.ToString("yy-mm-dd"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+
+// decimal valorMonetario = 82.40M;
+
+// Console.WriteLine(valorMonetario.ToString("N3"));
+
+// double porcentagem = .3423;
+
+// Console.WriteLine(porcentagem.ToString("P"));
+
+
+// int numero = 123456;
+
+// Console.WriteLine(numero.ToString("##-##-##"));
+
+
+
+
+
+
+
+
 
 
 // string numero1 = "10";
